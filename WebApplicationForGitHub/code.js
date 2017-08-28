ab027723690c37252a553de4a633285bde23f8a1
FIRM\KristinaZ <kristinafrench7@gmail.com>
2017-08-28 16:34:01 +0300

    Changed fetches

--- WebApplicationForGitHub/code.js
+++ WebApplicationForGitHub/code.js
@@ -1,172 +1,181 @@
 ﻿$(document).ready(function () {
     $('#tab-bar a').click(function () {
-        var url = $(this).attr('href');
-
-        $.ajax({
-            url: url + '?ajax=1',
-            success: function (r) {
-                $('#content').html(r);
-            }
-        });
+        let url = $(this).attr('href');
 
         if (url !== window.location) {
             window.history.pushState(null, null, url);
         }
 
-        tabChanged(url);
+        let id = $(this).attr('id');
+        tabChanged(id);
 
         return false;
     });
-
-    $(window).bind('popstate', function () {
-        $.ajax({
-            url: location.pathname + '?ajax=1',
-            success: function (r) {
-                $('#content').html(r);
-            }
-        });
-    });
 });
 
-function tabChanged(url) {
+function tabChanged(typeOfTab) {
     clear();
+    checkTabs(typeOfTab);
+    let url = createUrl(typeOfTab);
+    let doAfter = chooseDoAfter(typeOfTab);
 
-    let type = url.replace("/", "");
-    document.getElementById(type + "Link").className += " mdc-tab--active";
-
-    getInfo(type);
+    getRequest(url)
+        .then(function (response) {
+            doAfter(response);
+        });
 }
 
 function clear() {
-    let section = document.getElementById("issues");
+    let section = document.getElementById("section");
     if (section) {
-        document.getElementById("issuesLink").className = "mdc-tab";
         document.body.removeChild(section);
-        return;
-    }
-
-    section = document.getElementById("commits");
-    if (section) {
-        document.getElementById("commitsLink").className = "mdc-tab";
-        document.body.removeChild(section);
-        return;
-    }
-
-    section = document.getElementById("notifications");
-    if (section) {
-        document.getElementById("notificationsLink").className = "mdc-tab";
-        document.body.removeChild(section);
-        return;
     }
 }
 
-function getInfo(type) {
-    fetch("static.txt", {
-        headers: {
-            "Content-Type": "application/json"
-        }
-    }).then(function (response) {
-        if (response.status === 200) {
-            return response.text();
-        }
-    }).then(function (text) {
-        return JSON.parse(text);
-    }).then(function (info) {
-        showSection(info, type);
-    }).catch(function (ex) {
-        alert('error');
-    });
+function checkTabs(activeTab) {
+    if (activeTab !== "issues")
+        doTabNotActive("issues");
+
+    if (activeTab !== "commits")
+        doTabNotActive("commits");
+
+    if (activeTab !== "notifications")
+        doTabNotActive("notifications");
+
+    let tab = document.getElementById(activeTab);
+    tab.className += " mdc-tab--active";
 }
 
-function showSection(info, type) {
-    let url = createUrl(info, type);
-
-    let doAfter;
-
-    if (type === 'issues')
-        doAfter = showIssues;
-    else {
-        if (type === 'commits')
-            doAfter = showCommits;
-        else
-            doAfter = showNotifications;
-    }
-
-    getRequest(url, info.token, doAfter);
+function doTabNotActive(tab) {
+    document.getElementById(tab).className = "mdc-tab";
 }
 
-function createUrl(info, type) {
+function createUrl(type) {
     let url = "https://api.github.com/";
 
-    if (type !== 'notifications') {
-        url += 'repos/' + info.owner + '/' + info.repo + '/';
+    if (type === 'issues') {
+        url += 'repos/' + info.owner + '/' + info.repo + '/issues';
     }
 
-    url += type;
+    if (type === 'commits') {
+        url += 'repos/' + info.owner + '/' + info.repo + '/commits';
+    }
+
+    if (type === 'notifications') {
+        url += 'notifications';
+    }
 
     return url;
 }
 
-function getRequest(url, token, doAfter) {
-    let valueOfHeader = "token " + token;
+function chooseDoAfter(type) {
+    let doAfter;
 
-    fetch(url, {
+    if (type === 'issues') {
+        doAfter = createListIssues;
+    }
+
+    if (type === 'commits') {
+        doAfter = createListCommits;
+    }
+
+    if (type === 'notifications') {
+        doAfter = createListNotifications;
+    }
+
+    return doAfter;
+}
+
+function getRequest(url) {
+    let valueOfHeader = "token " + info.token;
+
+    return fetch(url, {
         headers: {
             "Content-Type": "application/json",
             "Authorization": valueOfHeader
         }
-    }).then(function (response) {
-        if (response.status === 200) {
+    })
+        .then(function (response) {
             return response.json();
-        }
-    }).then(function (response) {
-        doAfter(response);
-    }).catch(function (ex) {
-        alert('error');
-    });
+        }).catch(function (ex) {
+            alert('error');
+        });
 }
 
-function showIssues(response) {
-    let section = document.createElement('section');
-    section.id = "issues";
-    document.body.appendChild(section);
+function createListIssues(response) {
+    let list = new Array(10);
 
-    let ul = document.createElement('ul');
-    ul.className = "mdc-list";
-    section.appendChild(ul);
+    let len = response.length;
+    let i = 0;
+    let successful = 0;
+
+    while (i < len && successful < 10) {
+        if (response[i].state === "open") {
+            list[successful] = {
+                'url': response[i]['html_url'],
+                'text': response[i].title
+            }
+            successful++;
+        }
+
+        i++;
+    }
+
+    list.length = successful;
+
+    showTextList(list);
+}
+
+function createListCommits(response) {
+    let list = new Array(10);
 
     let len = response.length;
     let i = 0;
 
     while (i < len && i < 10) {
-        if (response[i].state === "open") {
-            let li = document.createElement('li');
-            li.className = "mdc-list-item";
-            ul.appendChild(li);
-
-            let link = document.createElement('a');
-            link.href = response[i]['html_url'];
-            li.appendChild(link);
-
-            let p = document.createElement('p');
-            p.innerText = response[i].title;
-            p.className = "listTextItem mds-typography--headline";
-            link.appendChild(p);
+        list[i] = {
+            'url': response[i]['html_url'],
+            'text': response[i].commit.committer.date + ' - ' + response[i].commit.committer.name + "\n" + response[i].commit.message
         }
         i++;
     }
+
+    list.length = i;
+
+    showTextList(list);
 }
 
-function showCommits(response) {
+function createListNotifications(response) {
+    let list = new Array(10);
+
+    let len = response.length;
+    let i = 0;
+
+    while (i < len && i < 10) {
+        list[i] = {
+            'reason': response[i].reason,
+            'url': response[i].subject.url.replace("api.", "").replace("repos/", ""),
+            'text': response[i].subject.title
+        }
+
+        i++;
+    }
+
+    list.length = i;
+
+    showTextListWithAvatar(list);
+}
+
+function showTextList(list) {
     let section = document.createElement('section');
-    section.id = "commits";
+    section.id = "section";
     document.body.appendChild(section);
 
     let ul = document.createElement('ul');
     ul.className = "mdc-list";
     section.appendChild(ul);
 
-    let len = response.length;
+    let len = list.length;
     let i = 0;
 
     while (i < len && i < 10) {
@@ -175,54 +184,51 @@ function showCommits(response) {
         ul.appendChild(li);
 
         let link = document.createElement('a');
-        link.href = response[i]['html_url'];
+        link.href = list[i].url;
         li.appendChild(link);
 
         let p = document.createElement('p');
-        p.innerText = response[i].commit.committer.date + ' - ' + response[i].commit.committer.name + "\n" + response[i].commit.message;
-        p.className = "listTextItem textTwoLines mds-typography--headline";
+        p.innerText = list[i].text;
+        p.className = "listTextItem mds-typography--headline textTwoLines";
         link.appendChild(p);
-
         i++;
     }
 }
 
-function showNotifications(response) {
+function showTextListWithAvatar(list) {
     let section = document.createElement('section');
-    section.id = "notifications";
+    section.id = "section";
     document.body.appendChild(section);
 
     let ul = document.createElement('ul');
     ul.className = "mdc-list mdc-list--avatar-list";
     section.appendChild(ul);
 
-    let len = response.length;
+    let len = list.length;
     let i = 0;
 
     while (i < len && i < 10) {
-        if (response[i].unread === true) {
-            let li = document.createElement('li');
-            li.className = "mdc-list-item withMargin";
-            ul.appendChild(li);
+        let li = document.createElement('li');
+        li.className = "mdc-list-item withMargin";
+        ul.appendChild(li);
 
-            let img = document.createElement('img');
-            img.className = "mdc-list-item__start-detail";
+        let img = document.createElement('img');
+        img.className = "mdc-list-item__start-detail";
 
-            if (response[i].reason === "assign")
-                img.src = "/images/assign.png";
-            else img.src = "/images/mention.png";
+        if (list[i].reason === "assign")
+            img.src = "/images/assign.png";
+        else img.src = "/images/mention.png";
 
-            li.appendChild(img);
+        li.appendChild(img);
 
-            let link = document.createElement('a');
-            link.href = response[i].subject.url.replace("api.", "").replace("repos/", "");
-            li.appendChild(link);
+        let link = document.createElement('a');
+        link.href = list[i].url;
+        li.appendChild(link);
 
-            let p = document.createElement('p');
-            p.innerText = response[i].subject.title;
-            p.className = "listTextItem mds-typography--headline";
-            link.appendChild(p);
-        }
-        i++;
+        let p = document.createElement('p');
+        p.innerText = list[i].text;
+        p.className = "listTextItem mds-typography--headline";
+        link.appendChild(p);
     }
+    i++;
 }
---
+++ WebApplicationForGitHub/info.js
@@ -0,0 +1,5 @@
+﻿var info = {
+	"token": "112282ef5dba3c6b284e4e0b3e0a9b1db3f5a092",
+	"owner": "Confirmit",
+	"repo": "Portal"
+};
